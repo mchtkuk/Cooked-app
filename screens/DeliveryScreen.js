29649140b674d6fcfa361292bef55367a9b18d84
mchtkk <mucahit.kuk@gmail.com>
2023-10-20 17:20:46 +0300

    map and pin

--- screens/DeliveryScreen.js
+++ screens/DeliveryScreen.js
@@ -10,12 +10,16 @@ import { useNavigation } from "@react-navigation/native";
 import { useSelector } from "react-redux";
 import { selectRestaurant } from "../redux/restaurantSlice";
 import { XMarkIcon } from "react-native-heroicons/outline";
+import MapView from "react-native-maps";
+import {Marker} from 'react-native-maps';
 
 
 const DeliveryScreen = () => {
   const restaurant = useSelector(selectRestaurant);
 
   const navigation = useNavigation();
+
+  console.log(restaurant.lat, restaurant.long);
 
   return (
     <View className="bg-orange-500 flex-1">
@@ -30,10 +34,33 @@ const DeliveryScreen = () => {
           <View>
             <Text className="text-lg text-gray-400">Estimated Arrival</Text>
             <Text className="text-4xl font-bold mt-2">45-55 Minutes</Text>
+            <Text className="mt-3 text-gray-500">
+              Your order at {restaurant.title} being prepared
+            </Text>
           </View>
         </View>
-        
       </SafeAreaView>
+      <MapView
+        initialRegion={{
+          latitude: restaurant.lat,
+          longitude: restaurant.long,
+          latitudeDelta: 0.005,
+          longitudeDelta: 0.005,
+        }}
+       className="flex-1 -mt-10 z-0"
+        mapType="mutedStandard"
+      >
+        <Marker
+          coordinate={{
+            latitude: restaurant.lat,
+            longitude: restaurant.long,
+          }}
+          title={restaurant.title}
+          description={restaurant.short_description}
+          identifier="origin"
+          pinColor="red"
+        />
+      </MapView>
     </View>
   );
 };
